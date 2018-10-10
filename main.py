c4c04cb6f77dc718308f35f1eaf38969f25df8d5
Marcelo Cobias <marcelocobias18@gmail.com>
2018-10-10 20:28:58 -0300

    Remove files and add method: compute_graph.

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
 import cv2
+import numpy as np
+from time import time
 from utils.face_find import detect_face_align
 from utils.mysql_connect import get_mysql_images
 from utils.split_data import train_test_split_images
 from utils.split_data import cross_validation_split_images
 from utils.image_util import read_training_data
 
+test_percent = 0.2
 
-print("Preparing data...")
+#start time
+t0 = time()
+print("--------------------")
+print("   Preparing data   ")
+print("--------------------")
 
-#test = cv2.imread('/media/psf/Home/Documents/Mestrado/Dataset faces/FRGC-2.0-dist/nd1/Fall2002/2002-247/04514d04.JPG')
-#face = detect_face_align(test, 512)
-#cv2.imshow('Face', face)
-#cv2.waitKey(0)
+db_list = get_mysql_images('frgc')
 
-list = get_mysql_images('frgc')
-read_training_data(list, '/media/psf/Home/Documents/Mestrado/Dataset faces/', 32)
-#print(list[0])
+train_data, test_data =  train_test_split_images(db_list, test_percent)
+#train_data, test_data = cross_validation_split_images(list, 2)
 
+faces_train, label_train, image_name_train = read_training_data(train_data, '/media/psf/Home/Documents/Mestrado/Dataset faces/', 16)
+faces_test, label_test, image_name_test = read_training_data(test_data, '/media/psf/Home/Documents/Mestrado/Dataset faces/', 16)
 
+print("--------------------")
+print("   Data prepared   ")
+print("--------------------")
 
-#train_data, test_data =  train_test_split_images(list, 0.2)
-#print(len(train_data))
-#cross_validation_split_images(list, 2)
+print("--------------------")
+print("Total dataset size:")
+print("Number of samples in dataset: %d" % len(db_list))
+print("Number of classes in dataset: %d" % len(image_name_train))
+print("--------------------")
+print("Total train size: %d samples, %d%%" % (len(train_data), (1 - test_percent) * 100))
+print("Total test size: %d samples, %d%%" % (len(test_data), test_percent * 100))
+#print("Cross validation %d-fold" % cross_validation)
+print("--------------------")
+
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+face_recognizer.train(faces_train, np.array(label_train))
+
+print("done in %0.3fs" % (time() - t0))
+
+def predict(img_face):
+    # predict the image using our face recognizer
+    label, confidence = face_recognizer.predict(img_face)
+    # get name of respective label returned by face recognizer
+    label_text = label_train[label]
+
 
 
 
--- utils/image_util.py
+++ utils/image_util.py
@@ -1,22 +1,36 @@
 import cv2
 import os
 import numpy as np
+import imghdr
 from utils.face_find import detect_face_align
 
 def read_training_data(list_images, data_folder, size):
     faces_train = []
+    label_train = []
+    image_name_train = []
     for image_name in list_images:
-        if image_name[15].startswith("."):
+        if(is_valid_image(data_folder + image_name[15])):
             continue
 
-        print(data_folder + image_name[15])
         image = cv2.imread(data_folder + image_name[15])
-
-        print("Detecting face in image: " + image_name[1])
-
+        print("Image  <- " + image_name[1])
         face = detect_face_align(image, size)
-        print("Training Label: " + image_name[0])
+        print("Person <- " + image_name[0])
+        print("--")
 
         if face is not None:
-            faces_train.append((image_name[0], face))
-    return faces_train
+            faces_train.append(face)
+            label_train.append(image_name[0])
+            image_name_train.append(image_name[1])
+    return faces_train, label_train, image_name_train
+
+def is_valid_image(str_image):
+    ext = imghdr.what(str_image)
+    if(ext == "jpeg"):
+        return False
+    elif(ext == "png"):
+        return False
+    elif(ext == "ppm"):
+        return False
+    else:
+        return True
